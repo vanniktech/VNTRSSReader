f9352025869b5b6e3e687d186442680e6e41d780
[L] Niklas Baudy <niklas.baudy@vanniktech.de>
2014-07-24 17:17:12 +0200

    Atom updated tag will be handled as pubDate

--- VNTRSSReader/vntrssreader.cpp
+++ VNTRSSReader/vntrssreader.cpp
@@ -101,7 +101,7 @@ void VNTRSSReader::replyFinished(QNetworkReply* networkReply) {
 
             if (prefix == "dc" || (prefix == "media" && name == "title") || (prefix == "atom10" && name == "link")) continue;
             else if (atom && (name == "summary" || name == "subtitle")) name = "description";
-            else if (atom && name == "published") name = "pubDate";
+            else if (atom && (name == "published" || name == "updated")) name = "pubDate";
             else if (atom && name == "id") name = "guid";
             else if (prefix == "media" && name == "thumbnail") name = "imageUrl";
 
